8082717c4ad85b978babe4b3b68b5a78adad8cdc
Ryu Yu <ryuyu@microsoft.com>
2016-03-17 15:39:25 -0700

    added comment descripting io streams

--- cli/script/command-executor.ts
+++ cli/script/command-executor.ts
@@ -997,7 +997,7 @@ export var releaseCordova = (command: cli.IReleaseCordovaCommand): Promise<void>
 
     log(chalk.cyan("Running \"cordova prepare\" command:\n"));
     try {
-        var prepareProcess: any = spawnSync("cordova", ["prepare", platform, "--verbose"], { stdio: [0, 1, 2] });
+        var prepareProcess: any = spawnSync("cordova", ["prepare", platform, "--verbose"], { stdio: [0, 1, 2] /* Inherit all io streams */ });
         if (prepareProcess.error) {
             throw prepareProcess.error;
         }
